c712907a3728d06ba85d17da6a2b7e99ab277856
Elisabeth Henkens <ellie@henkens.com>
2021-12-08 12:47:11 -0500

    Refined segments.py

--- Day 8/segments.py
+++ Day 8/segments.py
@@ -1,87 +1,38 @@
 from typing import List
 
-zero = [1, 1, 1, 0, 1, 1, 1]
-one = [0, 0, 1, 0, 0, 1, 0]
-two = [1, 0, 1, 1, 1, 0, 1]
-three = [1, 0, 1, 1, 0, 1, 1]
-four = [0, 1, 1, 1, 0, 1, 0]
-five = [1, 1, 0, 1, 0, 1, 1]
-six = [1, 1, 0, 1, 1, 1, 1]
-seven = [1, 0, 1, 0, 0, 1, 0]
-eight = [1, 1, 1, 1, 1, 1, 1]
-nine = [1, 1, 1, 1, 0, 1, 1]
-
 def getData() -> List[str]:
-
     inp = open("input.txt", "r")
-
     data = []
-
     for line in inp:
         data.append(line.strip())
 
     return data
 
-def filterMapping(mapping: str, signal: str) -> str:
-
-    if mapping == "":
-        return signal
-
-    newMapping = ""
-
-    for char in signal:
-        if char in mapping:
-            newMapping += char
-
-    return newMapping
-
-def countSharedWires(mappings: List[str], segments: List[int], signal: str) -> int:
-
+def countSharedWires(signal1: str, signal2: str) -> int:
     wires = 0
-
-    for char in signal:
-        i = 0
-        while i < 7:
-            if segments[i] == 1 and char in mappings[i]:
-                wires += 1
-                break
-            i += 1
+    for char in signal1:
+        if char in signal2:
+            wires += 1
 
     return wires
 
 def matchWirings(line: str) -> int:
-
     line = line.split(" | ")
     signals = line[0].split()
     numbers = line[1].split()
 
-    mappings = [""] * 7
+    one = ""
+    four = ""
 
-    # Fill in the uniques mappings
     for signal in signals:
-
-        segments = None
-
         length = len(signal)
 
         if length  == 2:
-            segments = one
-        elif length == 3:
-            segments = seven
+            one = signal
         elif length == 4:
-            segments = four
-        elif length == 7:
-            segments = eight
-        else:
-            continue
-
-        for i in range(0, 7):
-            if segments[i] == 1:
-                mappings[i] = filterMapping(mappings[i], signal)
-
+            four = signal
     
     output = ""
-
     for signal in numbers:
         length = len(signal)
 
@@ -94,16 +45,16 @@ def matchWirings(line: str) -> int:
         elif length == 7:
             output += "8"
         elif length == 6:
-            if not (countSharedWires(mappings, one, signal) == 2):
+            if not (countSharedWires(one, signal) == 2):
                 output += "6"
-            elif not (countSharedWires(mappings, four, signal) == 4):
+            elif not (countSharedWires(four, signal) == 4):
                 output += "0"
             else:
                 output += "9"
         elif length == 5:
-            if countSharedWires(mappings, one, signal) == 2:
+            if countSharedWires(one, signal) == 2:
                 output += "3"
-            elif countSharedWires(mappings, four, signal) == 3:
+            elif countSharedWires(four, signal) == 3:
                 output += "5"
             else:
                 output += "2"
